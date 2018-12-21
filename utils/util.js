ce7e2b5f4510575bc5e4edb4ac4187fdc1f79c55
vankasteelj <vankasteelj@gmail.com>
2018-12-21 16:55:22 +0100

    bugfix: passing a null/undefined id
    - the id was only tested for its key, not it's value.

--- utils/util.js
+++ utils/util.js
@@ -2,7 +2,7 @@ module.exports = {
     inArray: (obj, arr) => {
         for (let value of arr) {
             for (let key in obj) {
-                if (key == value) return true
+                if (key == value && obj[key]) return true
             }
         }
         return false
